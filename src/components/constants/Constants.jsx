5d4f3b0f3a3ce2a20631e485279b0d6638a385ea
githubodo <tvmalaysia@protonmail.com>
2025-04-06 00:28:07 +0800

    update

--- src/components/constants/Constants.jsx
+++ src/components/constants/Constants.jsx
@@ -15,12 +15,12 @@ import { FaUserTie } from "react-icons/fa";
 
 export const sidebarData = [
   { name: "Trending", icon: <AiTwotoneFire /> },
-  { name: "Adventure and Movies", icon: <BiMoviePlay /> },
+  { name: "Adveture and Movies", icon: <BiMoviePlay /> },
   { name: "Programming and Coding", icon: <BsCodeSlash /> },
   { name: "Science and Fact", icon: <MdScience /> },
   { name: "Technology and Space", icon: <FaSpaceAwesome /> },
   { name: "Business and Startups", icon: <FaUserTie /> },
-  { name: "Sports and Highlights", icon: <MdSportsBaseball /> },
+  { name: "Sports and Highlightes", icon: <MdSportsBaseball /> },
   { name: "Movies and Webseries", icon: <BiCameraMovie /> },
   { name: "AI and ChatGpt", icon: <GiBrain /> },
   { name: "Health and Fitness", icon: <MdOutlineHealthAndSafety /> },
@@ -30,34 +30,14 @@ export const sidebarData = [
 
 export const countries = [
   {
-    name: "Malaysia",
-    countryCode: "MY",
-    url: "https://media.istockphoto.com/id/1445564404/vector/vector-illustration-of-flat-round-shaped-of-malaysia-flag-official-national-flag-in-button.jpg?s=612x612&w=0&k=20&c=2iHWuPvYFHykB2Fwrzc_BHPtuKtQekGIbqvEn0gVgJk=",
-  },
-  {
-    name: "Indonesia",
-    countryCode: "ID",
-    url: "https://t4.ftcdn.net/jpg/07/72/42/37/360_F_772423770_U6fHIuldSK1Gj5fjyyM4CpfK3qovegWb.jpg",
-  },
-  {
-    name: "Singapore",
-    countryCode: "SG",
-    url: "https://img.freepik.com/premium-vector/circle-flag-logo-icon-computer-vector-illustration-design_1143296-3223.jpg?semt=ais_hybrid",
+    name: "India",
+    countryCode: "IN",
+    url: "https://t4.ftcdn.net/jpg/02/81/47/57/240_F_281475718_rlQONmoS2E3CJtv0zFv2HwZ1weGhxpff.jpg",
   },
   {
     name: "America",
     countryCode: "US",
     url: "https://t3.ftcdn.net/jpg/02/70/24/98/240_F_270249859_mf1Kyad7MO3Gb1BGvBahbB9SNttnVZO7.jpg",
-  },
-  {
-    name: "Canada",
-    countryCode: "CA",
-    url: "https://t3.ftcdn.net/jpg/01/71/57/72/240_F_171577280_Gj1SV9BV1vrvowWTexaiJW7OBj7uNgCT.jpg",
-  },
-  {
-    name: "England",
-    countryCode: "GB",
-    url: "https://t3.ftcdn.net/jpg/06/01/92/56/240_F_601925600_OPd3C0QuEE283YX2Fj6v3QtFFnkdtETF.jpg",
   },
   {
     name: "Germany",
@@ -70,8 +50,13 @@ export const countries = [
     url: "https://t3.ftcdn.net/jpg/01/79/73/80/240_F_179738020_0cdBcea7tUpPoFTCiiVfl6p9chD28tQz.jpg",
   },
   {
-    name: "India",
-    countryCode: "IN",
-    url: "https://t4.ftcdn.net/jpg/02/81/47/57/240_F_281475718_rlQONmoS2E3CJtv0zFv2HwZ1weGhxpff.jpg",
+    name: "Canada",
+    countryCode: "CA",
+    url: "https://t3.ftcdn.net/jpg/01/71/57/72/240_F_171577280_Gj1SV9BV1vrvowWTexaiJW7OBj7uNgCT.jpg",
+  },
+  {
+    name: "England",
+    countryCode: "GB",
+    url: "https://t3.ftcdn.net/jpg/06/01/92/56/240_F_601925600_OPd3C0QuEE283YX2Fj6v3QtFFnkdtETF.jpg",
   },
 ];
--- src/context/YoutubeContext.js
+++ src/context/YoutubeContext.js
@@ -119,3 +119,4 @@ export const ContextProvider = ({ children }) => {
     </YoutubeContext.Provider>
   )
 }
+
